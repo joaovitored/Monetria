c73d1f19339fe6bb5596c031744e1b71b4ebc947
João Vitor <jvtreloso@gmail.com>
2026-01-12 23:52:23 -0300

    Update

--- FinTrack/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace FinTrack.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
---
+++ Monetria/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Monetria.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
